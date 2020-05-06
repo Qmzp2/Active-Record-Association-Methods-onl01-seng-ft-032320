94fa6fd461a0f0b7b4ddead7088b1c54733deedb
Kevin Mathias <komathias1@gmail.com>
2020-05-06 19:30:58 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -15,6 +15,6 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    self.songs.genre.uniq.count
+    self.genres.uniq.count
   end
 end
